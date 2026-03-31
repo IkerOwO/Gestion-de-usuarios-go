26caacca7af95bcff5fe6a4ec1f5b0003eaa564d
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-03-31 13:25:05 +0200

    Inicio de proyecto

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,33 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"GestionDeUsuarios/cmd/internal"
+)
+
+/*
+	Para el User Input se usa scan, scanf y scanln
+
+	scan: Recive el input con formato raw como valores separados con espacios. Las NewLines cuentan como espacios
+
+	scanf: Recive el input y lo guarda basado en determinados formatos 
+
+	scanln: Similar a scan pero para de escanear cuando se le da al enter
+*/
+
+func printMenu(){
+
+}
+
+func crearUsuarios(){
+	// Declaramos el usuario
+	var usuario internal.Usuario
+
+
+}
+
 
 func main(){
 	fmt.Println("Gestion de Usuarios")
-	
+
 }
---
+++ cmd/internal/usuarios.go
@@ -0,0 +1,19 @@
+package internal
+
+// Estructura de un usuario
+type Usuario struct {
+	nombreUsuario string
+	contraseña    string
+	fechaAlta     string
+}
+
+// Funcion guardar usuarios
+func guardarUsuarios()
+
+
+// Funcion banear usuarios
+func usuarioBaneado(usuario string) bool
+
+
+// Funcion comprobar si un usuario esta baneado
+func isUsuarioBaneado(usuario string) bool
