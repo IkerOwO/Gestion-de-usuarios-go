dde24242b4673cb51328cebe0df2ce905db749e0
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-04-02 11:36:08 +0200

    Funcion de crear usuario movida al otro archivo

--- cmd/internal/usuarios.go
+++ cmd/internal/usuarios.go
@@ -1,5 +1,10 @@
 package internal
 
+import (
+	"fmt"
+	"time"
+)
+
 // Estructura de un usuario
 type Usuario struct {
 	NombreUsuario string
@@ -10,6 +15,24 @@ type Usuario struct {
 
 // Array de usuarios
 var UsuariosArray []Usuario
+
+// Crear Usuarios
+func CrearUsuario(nombreUsuario string, contraseña string, fechaAlta string){
+	// Guardamos el usuario
+	nuevoUsuario := Usuario{
+		NombreUsuario: nombreUsuario,
+		Contraseña:    contraseña,
+		FechaAlta:     fechaAlta,
+		Baneado:       false,
+	}
+
+	// Guardamos el usuario en el array
+	UsuariosArray = append(UsuariosArray, nuevoUsuario)
+	
+	fmt.Print("Usuario creado")
+	time.Sleep(2 * time.Second)
+	fmt.Print("\033[H\033[2J")
+}
 
 // Funcion banear usuarios
 func BanUser(usuario string, usuarios []Usuario) bool {
